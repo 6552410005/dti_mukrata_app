7825498b67948e2f058a5c4a250e766466bb923c
6552410005 <s6552410005@sau.ac.th>
2024-04-02 13:13:39 +0700

    V1.3

--- lib/views/calculate_pay_bill_ui.dart
+++ lib/views/calculate_pay_bill_ui.dart
@@ -1,7 +1,8 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_final_fields, sort_child_properties_last
 
 import "package:flutter/material.dart";
 import "package:flutter/services.dart";
+import "package:font_awesome_flutter/font_awesome_flutter.dart";
 
 class CalculatePayBillUI extends StatefulWidget {
   const CalculatePayBillUI({super.key});
@@ -11,6 +12,14 @@ class CalculatePayBillUI extends StatefulWidget {
 }
 
 class _CalculatePayBillUIState extends State<CalculatePayBillUI> {
+  List<String> _mList = [
+    'ไม่เป็นสมาชิก',
+    'สมาชิก Silver Member ลด 5%',
+    'สมาชิก Gold Member ลด 10%',
+    'สมาชิก Platinum Member ลด 20%',
+  ];
+  String _member = 'ไม่เป็นสมาชิก';
+
   int pl = 1;
   String fd = 'นํ้าดื่ม';
   bool? adultStatus = false;
@@ -18,6 +27,7 @@ class _CalculatePayBillUIState extends State<CalculatePayBillUI> {
   bool? waterStatus = false;
   TextEditingController adultCtrl = TextEditingController(text: '');
   TextEditingController kidCtrl = TextEditingController(text: '');
+  TextEditingController cokeCtrl = TextEditingController(text: '');
   TextEditingController waterCtrl = TextEditingController(text: '');
 
   FocusNode focusNode = FocusNode();
@@ -75,6 +85,7 @@ class _CalculatePayBillUIState extends State<CalculatePayBillUI> {
                     onChanged: (param) {
                       setState(() {
                         adultStatus = param;
+                        adultCtrl.text = '';
                       });
                     },
                     value: adultStatus,
@@ -106,7 +117,7 @@ class _CalculatePayBillUIState extends State<CalculatePayBillUI> {
                         ],
                         enabled: adultStatus,
                         textAlign: TextAlign.center,
-                        focusNode: focusNode,
+                        //focusNode: focusNode,
                         decoration: InputDecoration(
                           border: UnderlineInputBorder(),
                           floatingLabelBehavior: FloatingLabelBehavior.never,
@@ -142,6 +153,7 @@ class _CalculatePayBillUIState extends State<CalculatePayBillUI> {
                     onChanged: (param) {
                       setState(() {
                         kidStatus = param;
+                        kidCtrl.text = '';
                       });
                     },
                     value: kidStatus,
@@ -173,7 +185,7 @@ class _CalculatePayBillUIState extends State<CalculatePayBillUI> {
                         ],
                         enabled: kidStatus,
                         textAlign: TextAlign.center,
-                        focusNode: focusNode,
+                        //focusNode: focusNode,
                         decoration: InputDecoration(
                           border: UnderlineInputBorder(),
                           floatingLabelBehavior: FloatingLabelBehavior.never,
@@ -205,7 +217,7 @@ class _CalculatePayBillUIState extends State<CalculatePayBillUI> {
               ),
               Padding(
                 padding: EdgeInsets.only(
-                    right: MediaQuery.of(context).size.width * 0.47),
+                    right: MediaQuery.of(context).size.width * 0.50),
                 child: Text(
                   'บุปเฟต์นํ้าดื่ม',
                   style: TextStyle(
@@ -219,7 +231,7 @@ class _CalculatePayBillUIState extends State<CalculatePayBillUI> {
                   Row(
                     children: [
                       SizedBox(
-                        width: MediaQuery.of(context).size.width * 0.15,
+                        width: MediaQuery.of(context).size.width * 0.125,
                       ),
                       Row(
                         children: [
@@ -230,6 +242,8 @@ class _CalculatePayBillUIState extends State<CalculatePayBillUI> {
                               setState(() {
                                 fd = paramValue!;
                                 waterStatus = false;
+                                cokeCtrl.text = '';
+                                waterCtrl.text = '';
                               });
                             },
                           ),
@@ -243,7 +257,7 @@ class _CalculatePayBillUIState extends State<CalculatePayBillUI> {
                   Row(
                     children: [
                       SizedBox(
-                        width: MediaQuery.of(context).size.width * 0.15,
+                        width: MediaQuery.of(context).size.width * 0.125,
                       ),
                       Radio(
                         value: 'ไม่รับ',
@@ -262,7 +276,7 @@ class _CalculatePayBillUIState extends State<CalculatePayBillUI> {
                   ),
                   Row(
                     children: [
-                      SizedBox(width: MediaQuery.of(context).size.width * 0.28),
+                      SizedBox(width: MediaQuery.of(context).size.width * 0.25),
                       Padding(
                         padding: EdgeInsets.only(bottom: 2.5),
                         child: Text(
@@ -283,7 +297,7 @@ class _CalculatePayBillUIState extends State<CalculatePayBillUI> {
                           padding:
                               EdgeInsets.only(bottom: 3, right: 25, left: 15),
                           child: TextField(
-                            controller: waterCtrl,
+                            controller: cokeCtrl,
                             keyboardType: TextInputType.number,
                             inputFormatters: [
                               FilteringTextInputFormatter.allow(
@@ -292,7 +306,7 @@ class _CalculatePayBillUIState extends State<CalculatePayBillUI> {
                             ],
                             enabled: waterStatus,
                             textAlign: TextAlign.center,
-                            focusNode: focusNode,
+                            //focusNode: focusNode,
                             decoration: InputDecoration(
                               border: UnderlineInputBorder(),
                               hintText: hintText,
@@ -319,8 +333,170 @@ class _CalculatePayBillUIState extends State<CalculatePayBillUI> {
                       ),
                     ],
                   ),
+                  Row(
+                    children: [
+                      SizedBox(width: MediaQuery.of(context).size.width * 0.25),
+                      Padding(
+                        padding: EdgeInsets.only(bottom: 2.5),
+                        child: Text(
+                          'นํ้าเปล่า 15 บาท/ขวด จำนวน',
+                          style: TextStyle(
+                            fontWeight: FontWeight.normal,
+                            fontSize:
+                                MediaQuery.of(context).size.height * 0.018,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.02,
+                      ),
+                      Expanded(
+                        flex: 2,
+                        child: Padding(
+                          padding:
+                              EdgeInsets.only(bottom: 3, right: 25, left: 15),
+                          child: TextField(
+                            controller: waterCtrl,
+                            keyboardType: TextInputType.number,
+                            inputFormatters: [
+                              FilteringTextInputFormatter.allow(
+                                RegExp(r'^[0-9]+.?[0-9]*'),
+                              ),
+                            ],
+                            enabled: waterStatus,
+                            textAlign: TextAlign.center,
+                            //focusNode: focusNode,
+                            decoration: InputDecoration(
+                              border: UnderlineInputBorder(),
+                              hintText: hintText,
+                              hintStyle: TextStyle(
+                                fontSize:
+                                    MediaQuery.of(context).size.height * 0.020,
+                                color: Colors.grey,
+                              ),
+                              suffixIcon: Padding(
+                                padding: EdgeInsets.only(left: 25, top: 12),
+                                child: Text(
+                                  'ขวด',
+                                  style: TextStyle(
+                                    fontSize:
+                                        MediaQuery.of(context).size.height *
+                                            0.020,
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.02,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                        right: MediaQuery.of(context).size.width * 0.45),
+                    child: Text(
+                      'ประเภทสมาชิก',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: MediaQuery.of(context).size.height * 0.025,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      left: 45,
+                      right: 45,
+                    ),
+                    child: DropdownButton(
+                      value: _member,
+                      onChanged: (paramValue) {
+                        setState(() {
+                          _member = paramValue!;
+                        });
+                      },
+                      items: _mList
+                          .map((e) => DropdownMenuItem(
+                                value: e,
+                                child: Row(
+                                  children: [
+                                    Text(e),
+                                  ],
+                                ),
+                              ))
+                          .toList(),
+                      isExpanded: true,
+                    ),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.02,
+                  ),
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.1,
+                      ),
+                      Expanded(
+                        child: ElevatedButton.icon(
+                          onPressed: () {},
+                          icon: Icon(
+                            FontAwesomeIcons.moneyBill1Wave,
+                            color: Colors.white,
+                          ),
+                          label: Text(
+                            'คำนวณเงิน',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                            backgroundColor: Colors.deepOrange,
+                            fixedSize: Size.fromHeight(
+                              50.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.02,
+                      ),
+                      Expanded(
+                        child: ElevatedButton.icon(
+                          onPressed: () {
+                            setState(() {});
+                          },
+                          icon: Icon(
+                            Icons.cancel,
+                            color: Colors.white,
+                          ),
+                          label: Text(
+                            'ยกเลิก',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                              backgroundColor: Color.fromARGB(255, 43, 39, 39),
+                              fixedSize: Size.fromHeight(
+                                55.0,
+                              )),
+                        ),
+                      ),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.1,
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.08,
+                  ),
                 ],
-              )
+              ),
             ],
           ),
         ),
